4243fded4568658f5db0dbd8ad3c4e4b89d05212
yusufbestas <yusuf.bestas@etiya.com>
2020-03-12 22:49:41 +0300

    Proje oluşturma

---
+++ src/main/java/com/desingpatern/singleton/singleton/EagerInitializationSingleton.java
@@ -0,0 +1,7 @@
+package com.desingpatern.singleton.singleton;
+
+public class EagerInitializationSingleton {
+
+    private static final EagerInitializationSingleton eagerIns=new EagerInitializationSingleton();
+
+}
